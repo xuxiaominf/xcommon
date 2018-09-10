9f3491ea7ad3b90bc7dbce75bf0a16a9f8034b2c
xuxiaominf <xuxiaominf@126.com>
2018-09-10 21:41:48 +0800

    volatile

--- src/main/java/utils/xcommon/util/lock/test/t3volatile/TestObject.java
+++ src/main/java/utils/xcommon/util/lock/test/t3volatile/TestObject.java
@@ -1,19 +1,17 @@
 package utils.xcommon.util.lock.test.t3volatile;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 public class TestObject  {
 //	private int state = 0;
-//	private volatile int state = 0;
-	private AtomicInteger state = new AtomicInteger(0);
+	private volatile int state = 0;
+//	private AtomicInteger state = new AtomicInteger(0);
 
 	public int getState() {
-//		return state;
-		return state.get();
+		return state;
+//		return state.get();
 	}
 
 	public void setState(int st) {
-		state.compareAndSet(state.get(), st);
-//		this.state = state;
+//		state.compareAndSet(state.get(), st);
+		this.state = state;
 	}
 }
