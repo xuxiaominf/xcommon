d16c4d396b427061d07d01a17e67e68fbaa13529
xiaoming3 <xiaoming3.xu@56qq.com>
2019-01-25 18:05:09 +0800

    clas文件解读
    Signed-off-by: xiaoming3 <xiaoming3.xu@56qq.com>

--- src/main/java/utils/xcommon/util/base/classfile/SupperClass.java
+++ src/main/java/utils/xcommon/util/base/classfile/SupperClass.java
@@ -4,12 +4,17 @@ import javax.xml.bind.annotation.XmlInlineBinaryData;
 
 @XmlInlineBinaryData
 public class SupperClass<R> extends BaseClass implements Comparable<Integer>{
-	private int i1=1;
+	private final int i1=1;
 	private static int ia1[]=new int[]{1,2};
 	private RefClass refClass;
 	
-	public synchronized int getI1(){
-		return i1;
+	public synchronized int getI1(Integer ... integers) throws Exception {
+		int aaa=34;
+		int bbb=340;
+		int ccc=250;
+		int ddd=489;
+		int eee = 987;
+		return eee-9;
 	}
 	public RefClass getRef(){
 		return refClass;
